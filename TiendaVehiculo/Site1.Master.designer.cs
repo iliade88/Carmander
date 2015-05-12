d4fb4b325dd5caf47c0f51a6d01904312b82835f
jst7 <jorge.segovia.tormo@gmail.com>
2015-05-12 16:05:51 +0200

    version 2

--- TiendaVehiculo/Site1.Master.designer.cs
+++ TiendaVehiculo/Site1.Master.designer.cs
@@ -56,14 +56,5 @@ namespace TiendaVehiculo {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder folderbajo;
-        
-        /// <summary>
-        /// Control ContentPlaceHolder1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
