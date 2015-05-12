1958a0e9a633e50319c1a797ddd3cceecd05039f
jst7 <jorge.segovia.tormo@gmail.com>
2015-05-12 16:18:00 +0200

    version 7 redefinido_topor_TiendaVehiculos

--- TiendaVehiculo/Default.aspx
+++ TiendaVehiculo/Default.aspx
@@ -1,4 +1,5 @@
 ﻿<%@ Page  Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TiendaVehiculo.Default" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 
--- TiendaVehiculo/Site1.Master.designer.cs
+++ TiendaVehiculo/Site1.Master.designer.cs
@@ -31,6 +31,15 @@ namespace TiendaVehiculo {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Control foldertop.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder foldertop;
+        
+        /// <summary>
         /// Control folderarriba.
         /// </summary>
         /// <remarks>
